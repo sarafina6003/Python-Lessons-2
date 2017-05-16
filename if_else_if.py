2bdb079f50a52d436e8ae6926b207068f7381d4e
sarafina <sarafinanyawira@gmail.com>
2017-05-16 10:07:17 +0300

    if statements in python

--- if_else_if.py
+++ if_else_if.py
@@ -4,3 +4,11 @@ if age <5 :
     print ("This is a toddler")
 elif age >=6 and age<=12:
     print ("This is a kid")
+elif age >=13 and age<=20:
+    print ("This is a teenager")
+elif age >=21 and age<=35:
+    print ("This is a youth")
+elif age >=36 and age<=55:
+    print ("This is a middle age")
+else:
+    print ("This is a senior citizen")
