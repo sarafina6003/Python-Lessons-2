6ba127753eda457bfbe0fd4442cbcc5643e0554f
sarafina <sarafinanyawira@gmail.com>
2017-05-16 10:37:31 +0300

    loops

--- if_else_if.py
+++ if_else_if.py
@@ -1,14 +1,16 @@
-age = 27
+age = input("Enter your age: ")
+
+age = int(age)
 
 if age <5 :
-    print ("This is a toddler")
+    print ("This is a toddler :"+str(age))
 elif age >=6 and age<=12:
-    print ("This is a kid")
+    print ("This is a kid :"+str(age))
 elif age >=13 and age<=20:
-    print ("This is a teenager")
+    print ("This is a teenager :"+str(age))
 elif age >=21 and age<=35:
-    print ("This is a youth")
+    print ("This is a youth :"+str(age))
 elif age >=36 and age<=55:
-    print ("This is a middle age")
+    print ("This is a middle age :"+str(age))
 else:
-    print ("This is a senior citizen")
+    print ("This is a senior citizen :"+str(age))
---
+++ loops.py
@@ -0,0 +1,8 @@
+#loops
+#while (while something is true, the loop runs)
+#for loop
+
+counter = 1
+while counter <100:
+    print(counter)
+    counter +=1
