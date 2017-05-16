b3d437b553ea1bf8fddb4f038196de7b11041b11
sarafina <sarafinanyawira@gmail.com>
2017-05-16 10:01:40 +0300

    if statements in python

---
+++ if_else_if.py
@@ -0,0 +1,6 @@
+age = 27
+
+if age <5 :
+    print ("This is a toddler")
+elif age >=6 and age<=12:
+    print ("This is a kid")
---
+++ if_statements.py
@@ -0,0 +1,8 @@
+#if statements
+username = 'jj@yahoo.com'
+password = '1234'
+
+if username == 'jk@yahoo.com' or password == '1234':
+    print("Username and password is correct")
+else:
+    print("Username or password is wrong")
